ae2de678d74dbb9974ab64e439278238b54a92af
Sherlockes <sherlockes@yahoo.es>
2020-05-01 23:15:18 +0200

    Update

--- bash/rclone.sh
+++ bash/rclone.sh
@@ -19,11 +19,7 @@ fi
 
 rclone_check(){
     if which rclone >/dev/null; then
-	echo 'Comprobando la versión instalada de Rclone...'
 	rclone_local_ver=$(rclone version | head -1 | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
-
-	echo 'Versión instalada '$rclone_local_ver' de Rclone, comprobando versión web...'
-			
 	rclone_web_ver=$(curl -s https://api.github.com/repositories/17803236/releases/latest \
 	    | grep "browser_download_url.*rclone-[^extended].*-linux-${bits}\.deb" \
     | cut -d ":" -f 2,3 \
@@ -32,14 +28,15 @@ rclone_check(){
 
 	if [ $rclone_local_ver == $rclone_web_ver ]
 	then
-	    echo "Su versión está actualizada"
+	    echo "rclone instalado y actualizado..."
 	else
-	    echo 'Actualizando a la versión '$rclone_web_ver
+	    echo 'actualizando a rclone '$rclone_web_ver
 	    sudo dpkg -P hugo
 	    rclone_install
 	fi
 	
     else
+	echo 'instalando rclone...'
 	rclone_install
     fi
 }
@@ -53,8 +50,6 @@ rclone_install(){
 	    | wget -qi -
 	
 	installer="$(find . -name "*linux-${bits}.deb")"
-	echo $installer
-	echo 'Instalando Rclone'
 	sudo dpkg -i $installer
 	rm $installer
 }
