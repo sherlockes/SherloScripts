5c2c475e168db950cb4c00e5e7a1ce78ff5ed9c4
Sherlockes <sherlockes@yahoo.es>
2020-05-12 01:11:47 +0200

    Update

---
+++ bash/hugo.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+###################################################################
+# Script Name: hugo.sh
+# Description: Instala y actualiza Hugo
+# Args: N/A
+# Creation/Update: 20191114/20200502
+# Author: www.sherblog.pro                                                
+# Email: sherlockes@gmail.com                                           
+###################################################################
+
+bits=$(getconf LONG_BIT)
+if [ $bits == '64' ]
+then
+    bits='64bit'
+else
+    bits='ARM'
+fi
+
+hugo_check(){
+    if which hugo >/dev/null; then
+	hugo_local_ver=$(hugo version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
+	hugo_web_ver=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest \
+			   | grep "browser_download_url.*hugo_[^extended].*_Linux-${bits}\.deb" \
+			   | cut -d ":" -f 2,3 \
+			   | tr -d \" \
+			   | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
+
+
+	if [ $hugo_local_ver == $hugo_web_ver ]
+	then
+	    echo "hugo instalado y actualizado..."
+	else
+	    echo 'actualizando a hugo '$hugo_web_ver
+	    sudo dpkg -P hugo
+	    hugo_install
+	fi
+	
+    else
+	echo 'Hugo no está instalado...'
+	hugo_install
+    fi
+}
+
+hugo_install(){
+    echo 'Descargando la última versión de Hugo...'
+	curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest \
+	| grep "browser_download_url.*hugo_[^extended].*_Linux-${bits}\.deb" \
+	| cut -d ":" -f 2,3 \
+	| tr -d \" \
+	| wget -qi -
+    installer="$(find . -name "*Linux-${bits}.deb")"
+    sudo dpkg -i $installer
+    rm $installer
+}
+
+hugo_check
