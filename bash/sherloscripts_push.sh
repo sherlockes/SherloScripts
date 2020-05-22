6de6be245772568f02cdf0977cfc98c6493a4b24
Sherlockes <sherlockes@yahoo.es>
2020-05-22 23:08:15 +0200

    Update

--- bash/sherloscripts_push.sh
+++ bash/sherloscripts_push.sh
@@ -1,8 +1,8 @@
 #Sube los cambios generados en la web a GitHub
-rclone sync -v Sherlockes_GD:/SherloScripts/bash/ /home/pi/SherloScripts/bash
+rclone copy -v Sherlockes_GD:/SherloScripts/ /home/pi/SherloScripts/
 
 cd ~/SherloScripts
 git add --all
 git commit -m "Update"
-git push -u origin master
+git push
 exit 0
