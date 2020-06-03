45d3f773354a1fb9b72275560db6ab7f8b22cada
Sherlockes <sherlockes@yahoo.es>
2020-06-04 00:00:57 +0200

    Update

---
+++ bash/rss_reader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###################################################################
+# Script Name: rss.sh
+# Description: Instala y actualiza Hugo
+# Args: N/A
+# Creation/Update: 20191114/20200502
+# Author: www.sherblog.pro                                                
+# Email: sherlockes@gmail.com                                           
+###################################################################
+
