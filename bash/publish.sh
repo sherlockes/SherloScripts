465dc8d9f1692cabca0681ef303b4f2a41c0c424
Sherlockes <sherlockes@yahoo.es>
2020-08-02 00:00:30 +0200

    Update

--- bash/publish.sh
+++ bash/publish.sh
@@ -4,78 +4,66 @@
 ###########################################################################
 # Script Name: Actualización de www.sherblog.pro
 # Description:
-#	- Actualiza Hugo (
+#	- Actualiza Hugo (Eliminado por actualización diaria aparte)
 #	- Sincroniza Google Drive con las carpetas locales
 #	- Añade una cabecera a los archivos que no la tienen
 #	- Actualiza los archivos de la nube a los nuevos con cabecera
 #	- Genera la web estática
 #	- Sube la web a GitHub
 # Args: N/A
-# Creation/Update: 20180901/20191114
+# Creation/Update: 20180901/20200801
 # Author: www.sherblog.pro                                                
 # Email: sherlockes@gmail.com                                           
 ############################################################################
 
-#Actualiza Hugo a la última versión
-#curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest \
-#| grep "browser_download_url.*hugo_[^extended].*_Linux-ARM\.deb" \
-#| cut -d ":" -f 2,3 \
-#| tr -d \" \
-#| wget -qi -
-#installer="$(find . -name "*Linux-ARM.deb")"
-#sudo dpkg -i $installer
-#rm $installer
-
 # Comprueba si hay contenido para actualizar
 
 if rclone check --one-way Sherlockes_GD:/Sherblog/content/ /home/pi/sherblog/content/; then
-    echo command returned true
+    echo No hay cambios que actualizar...
 else
-    echo command returned some error
+    echo Actualizando los cambios...
+
+    # Sincroniza el contenido de la nube de Google Drive con las carpetas locales
+    rclone sync -v Sherlockes_GD:/Sherblog/content/ /home/pi/sherblog/content/
+    rclone sync -v Sherlockes_GD:/Sherblog/static/ /home/pi/sherblog/static/
+    rclone sync -v Sherlockes_GD:/Sherblog/layouts/ /home/pi/sherblog/layouts/
+
+    # Parsea los vertices para generar un gpx y genera la web estática en Hugo
+    cd ~/sherblog
+    ./parse_gpx.sh
+    /usr/local/bin/hugo
+
+    # Sube los cambios generados en la web a GitHub
+    cd ~/sherblog/sherlockes.github.io
+    git add --all
+    git commit -m "Update"
+    git push -u origin master
 fi
 
 
-
-
-#Sincroniza el contenido de la nube de Google Drive con las carpetas locales
-rclone sync -v Sherlockes_GD:/Sherblog/content/ /home/pi/sherblog/content/
-rclone sync -v Sherlockes_GD:/Sherblog/static/ /home/pi/sherblog/static/
-rclone sync -v Sherlockes_GD:/Sherblog/layouts/ /home/pi/sherblog/layouts/
-
-#Cambio al directorio de contenidos
+# Busca archivos sin cabecera para añadirle una genérica en "post"
 cd ~/sherblog/content/post
-
-#Busca archivos sin cabecera para añadirle una genérica
 grep -r -L "\-\-\-" * |
 while read fname
 do
-  #Minutos que han de pasar desde la ultima modificación para encabezado
+  # Minutos que han de pasar desde la ultima modificación para encabezado
   minmin=70
 
-  #Calculo de los minutos pasados desde la ultima moficicacion
+  # Calculo de los minutos pasados desde la ultima moficicacion
   umodsg=$(date +%s -r "$fname")
   ahorasg=$(date +%s)
   minpasados=$(((ahorasg - umodsg)/60))
   echo Han pasado $minpasados de los $minmin minutos desde la modificación de "$fname"
 
-  #Inserta la cabecera si han pasado más de 70 minutos
+  # Inserta la cabecera si han pasado más de 70 minutos
   if [ $minpasados -gt $minmin ]; then
     echo Colocando la cabecera a "$fname"
     cat cabecera.md "$fname" > temp && mv temp "$fname"
   fi
+
+  # Sincroniza los archivos de la nube con los modificados en local
+  rclone sync -v /home/pi/sherblog/content/post/ Sherlockes_GD:/Sherblog/content/post/
+
 done
 
-#Sincroniza los archivos de la nube con los modificados en local
-rclone sync -v /home/pi/sherblog/content/ Sherlockes_GD:/Sherblog/content/
-
-# Parsea los vertices para generar un gpx y genera la web estática en Hugo
-cd ~/sherblog
-./parse_gpx.sh
-/usr/local/bin/hugo
-
-#Sube los cambios generados en la web a GitHub
-cd ~/sherblog/sherlockes.github.io
-git add --all
-git commit -m "Update"
-git push -u origin master
 exit 0
