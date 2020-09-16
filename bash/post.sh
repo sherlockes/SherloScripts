503bbd25f855c1bc88e5574e1db1caea871275e4
Sherlockes <sherlockes@yahoo.es>
2020-09-17 00:00:25 +0200

    Update

--- bash/post.sh
+++ bash/post.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-###########################################################################
-# Script Name: Actualización de www.sherblog.pro
-# Description:
-#	- Actualiza Hugo (Eliminado por actualización diaria aparte)
-#	- Sincroniza Google Drive con las carpetas locales
-#	- Añade una cabecera a los archivos que no la tienen
-#	- Actualiza los archivos de la nube a los nuevos con cabecera
-#	- Genera la web estática
-#	- Sube la web a GitHub
-# Args: N/A
-# Creation/Update: 20180901/20200915
-# Author: www.sherblog.pro                                                
-# Email: sherlockes@gmail.com                                           
-############################################################################
-
+###################################################################
+#Script Name: post.sh
+#Description: - Genera un Post de Hugo de un archivo sin formato
+#             - Ubicado en "/home/pi/sherblog" de la Raspberry
+#             - Llamado por "publish.sh"
+#Args: N/A
+#Creation/Update: 20191022/20200915
+#Author: www.sherblog.pro                                                
+#Email: sherlockes@gmail.com                                           
+###################################################################
 
 dir_post=~/sherblog/content/post
 mins_mod=15
 
-add_header(){ 
+add_header(){
+    
+    
     lines_total=$(wc -l $1 | awk '{print $1}')
     lines_content=$(expr $lines_total - 2)
     echo $1 tiene $lines_total líneas con un contenido de $lines_content líneas.
     echo Colocando la cabecera a $1
-    # Asigna la primera línea a "short_title", elimina los espacios y los convierte en guión bajo.
-    short_title=$(head -n 1 $1 | sed 's/ /_/g') 
-    # Elimina los espacios y los convierte en guión bajo
-    #short_title="$(echo $short_title | sed 's/ /_/g')"
-
+    short_title=$(head -n 1 $1) # Asigna la primera línea a "short_title"
     echo El título será: $short_title
     long_title=$(sed '2q;d' $1) # Asigna la segunda línea a "long_title"
     echo El título largo será: $long_title
@@ -57,7 +50,7 @@ add_header(){
     echo "  - \"uncategorized\"" >> $file_name
     echo "tags:" >> $file_name
     echo "  - \"untagged\"" >> $file_name
-    echo "draft: true" >> $file_name
+    echo "draft: false" >> $file_name
     echo "weight: 5" >> $file_name
     echo "---" >> $file_name
     echo $summary >> $file_name
@@ -100,32 +93,5 @@ scan_posts(){
     done
 }
 
-# Comprueba si hay contenido para actualizar
-if rclone check --one-way Sherlockes_GD:/Sherblog/content/ /home/pi/sherblog/content/; then
-    echo No hay cambios que actualizar...
-else
-    echo Actualizando los cambios...
 
-    # Sincroniza el contenido de la nube de Google Drive con las carpetas locales
-    rclone sync -v Sherlockes_GD:/Sherblog/content/ /home/pi/sherblog/content/
-    rclone sync -v Sherlockes_GD:/Sherblog/static/ /home/pi/sherblog/static/
-    rclone sync -v Sherlockes_GD:/Sherblog/layouts/ /home/pi/sherblog/layouts/
 
-    # Parsea los vertices para generar un gpx y genera la web estática en Hugo
-    cd ~/sherblog
-    ./parse_gpx.sh
-    /usr/local/bin/hugo
-
-    # Sube los cambios generados en la web a GitHub
-    cd ~/sherblog/sherlockes.github.io
-    git add --all
-    git commit -m "Update"
-    git push -u origin master
-fi
-
-scan_posts
-
-# Sincroniza los archivos de la nube con los modificados en local
-rclone sync -v /home/pi/sherblog/content/ Sherlockes_GD:/Sherblog/content/
-
-exit 0
