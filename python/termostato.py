c6c43cbbb56be2a01d3e6b7a101a9b4fec5b4ff4
sherlockes <sherlockes@yahoo.es>
2020-11-12 00:00:35 +0100

    Update

--- python/termostato.py
+++ python/termostato.py
@@ -4,7 +4,7 @@
 # Description: Termostato inteligente a partir de una sonda dh22
 #              Y de un relé sonoff integrado mediante google sheets
 # Args: N/A
-# Creation/Update: 20201104/20201109
+# Creation/Update: 20201104/20201111
 # Author: www.sherblog.pro                                                
 # Email: sherlockes@gmail.com                                           
 ##################################################################
@@ -16,6 +16,7 @@ import ast
 from datetime import datetime
 from datetime import date
 import telegram
+import os
 
 ######################
 ## Captura de datos ##
@@ -51,29 +52,38 @@ hum_real = round(humidity,2)
 ## Parámetros de configuración para el Sonoff Mini ##
 #####################################################
 
-url = 'http://192.168.1.10:8081/zeroconf/switch'
-url_info = 'http://192.168.1.10:8081/zeroconf/info'
+sonoff_ip = "192.168.1.10"
+url = 'http://' + sonoff_ip + ':8081/zeroconf/switch'
+url_info = 'http://' + sonoff_ip + ':8081/zeroconf/info'
 rele_info = {"deviceid": "1000a501ef", "data": {}}
 url = 'http://192.168.1.10:8081/zeroconf/switch'
 rele_on = {"deviceid": "1000a501ef", "data": {"switch":"on"}}
 rele_off = {"deviceid": "1000a501ef", "data": {"switch":"off"}}
 
-# Estado actual del rele
-consulta=requests.post(url_info, json = rele_info)
-rele_estado = consulta.json()["data"]["switch"]
+# Comprobar si el relé está online y comprobar el estado
+response = os.system("ping -c 1 " + sonoff_ip)
+if response == 0:
+    print("El relé está conectado a la red")
 
-if consulta.json()["error"] == 0:
-    print(f"Estado del relé - OK ({rele_estado})")
+    # Estado actual del rele
+    consulta=requests.post(url_info, json = rele_info)
+    rele_estado = consulta.json()["data"]["switch"]
+
+    if consulta.json()["error"] == 0:
+        print(f"Estado del relé - OK ({rele_estado})")
+    else:
+        telegram.enviar("Algo falla en el relé de la calefacción")
+        print(f"Estado del relé - KO")
 else:
-    telegram.enviar("Algo falla en el relé de la calefacción")
-    print(f"Estado del relé - KO")
+    print("ATENCIÓN!!! El relé no está conectado a la red")
+    telegram.enviar("ATENCIÓN!!! El relé de la calefacción no está conectado a la red.")
 
 
 #####################
 ## Lazo de control ##
 #####################
 
-histeresis = 0.5
+histeresis = 0.3
 
 if temp_real < temp_consigna - histeresis and rele_estado == "off":
     print("Se va a encerder el relé")
@@ -101,16 +111,21 @@ else:
 
 hoja_datos = libro_gsheet.worksheet("datos")
 
-temp_ant_consigna = float(hoja_datos.acell('D2').value.replace(",", "."))
-temp_ant_real = float(hoja_datos.acell('E2').value.replace(",", "."))
-temp_ant_estado = hoja_datos.acell('G2').value
-hum_ant_real = float(hoja_datos.acell('F2').value.replace(",", "."))
+tiempo_ant = datetime.strptime(hoja_datos.acell('A2').value, '%Y/%m/%d %H:%M:%S')
+temp_ant_consigna = float(hoja_datos.acell('C2').value.replace(",", "."))
+temp_ant_real = float(hoja_datos.acell('D2').value.replace(",", "."))
+temp_ant_estado = hoja_datos.acell('F2').value
+hum_ant_real = float(hoja_datos.acell('E2').value.replace(",", "."))
 
 print(f"Anterior -> Tª consigna: {temp_ant_consigna} - Tª real:{temp_ant_real}ºC - Calef:{temp_ant_estado} - Humedad:{hum_ant_real}%")
 print(f"Actual   -> Tª consigna: {temp_consigna} - Tª real:{temp_real}ºC - Calef:{estado} - Humedad:{hum_real}%")
 
-if temp_real == temp_ant_real and temp_consigna == temp_ant_consigna and estado == temp_ant_estado:
+variacion = abs(temp_real - temp_ant_real) + abs(temp_consigna - temp_ant_consigna)
+print(f"Hay una variación de {variacion}")
+ahora = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+
+if variacion  < 0.2 and estado == temp_ant_estado:
     print("Nada ha cambiado (La humedad no cuenta...)")
 else:
-    hoja_datos.append_row([fecha,hora,temp_ext_real,temp_consigna,temp_real,hum_real,estado])
+    hoja_datos.append_row([ahora,temp_ext_real,temp_consigna,temp_real,hum_real,estado])
     hoja_datos.sort((1, 'des'), (2, 'des'), range='A2:AA106000')
