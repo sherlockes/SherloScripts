45809f8fa4ce164719bf588b45154ae708c2607e
sherlockes <sherlockes@yahoo.es>
2021-02-15 00:00:29 +0100

    Update

--- bash/teledown.sh
+++ bash/teledown.sh
@@ -3,7 +3,7 @@
 ###########################################################################
 # Script Name: Descarga de archivos desde una canal de Telegram
 # Description:
-# Creation/Update: 20210131/20210131
+# Creation/Update: 20210131/20210219
 # Author: www.sherblog.pro                                                
 # Email: sherlockes@gmail.com                                           
 ############################################################################
@@ -12,17 +12,10 @@
 . ~/config.conf
 
 rclone mount Sherlockes78_GD:Shd_Sherlockes78 ~/teledown/files --allow-other --daemon
-
 cd ~/teledown/telegram-download-daemon
-
 git pull origin master
-
 cd ..
-
-pip install -r telegram-download-daemon/requirements.txt
-
+sudo pip3 install -r telegram-download-daemon/requirements.txt
 python3 delete.py
-
-python3 telegram-download-daemon/telegram-download-daemon.py --api-id $api_id --api-hash $api_hash --channel $canal_descargas --dest ~/teledown/files --temp ~/teledown/files
-#python3 telegram-download-daemon-old/telegram-download-daemon.py --api-id $api_id --api-hash $api_hash --channel $canal_descargas --dest ~/teledown/files --temp ~/teledown/temp
+python3 telegram-download-daemon/telegram-download-daemon.py --api-id $api_id --api-hash $api_hash --channel $canal_descargas --dest ~/teledown/files --temp ~/teledown/temp
 
