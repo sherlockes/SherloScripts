84e4f43fc6fbddb429f7a58bd248060ddd85cec9
sherlockes <sherlockes@yahoo.es>
2021-11-22 17:41:37 +0100

    Update

--- bash/move2sherloflix.sh
+++ bash/move2sherloflix.sh
@@ -3,10 +3,13 @@
 ###########################################################################
 # Script Name: move2sherloflix.sh
 # Description: Busca archivo y carpetas para suber a la nube de sherloflix
-# Creation/Update: 20210428/20210428
+# Creation/Update: 20210428/20210814
 # Author: www.sherblog.pro                                                
 # Email: sherlockes@gmail.com                                           
 ############################################################################
+
+sherloflix="Onedrive_UN_en"
+conf_path="/var/services/homes/sherlockes/.config/rclone/rclone.conf"
 
 echo "---- Script para mover películas y series ----"
 
@@ -19,19 +22,19 @@ for i in * ; do
     echo "$i"
     if [ -d "$i" ]; then
 	echo "Moviendo ... $i"
-	rclone move -P --delete-empty-src-dirs "$i"/ Sherlockes78_UN_en:/series/"$i"
+        rclone move -P --config=$conf_path --delete-empty-src-dirs "$i"/ $sherloflix:/series/"$i"
     fi
 done
 
 
 # Moviendo las pelis de la carpeta 00_mover a la nube
-echo "1.- Moviendo peliculas...."
+echo "2.- Moviendo peliculas...."
 
 cd /volume1/video/Peliculas/00_mover
 
 for i in * ; do
-  if [ -d "$i" ]; then
+  if [ -f "$i" ]; then
       echo "Moviendo ... $i"
-      rclone move -P "$i" Sherlockes78_UN_en:/pelis/"$i"
+      rclone move -P --config=$conf_path "$i" $sherloflix:/pelis/"$i"
   fi
 done
