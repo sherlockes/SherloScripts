c97440925306b7828515583a95337535f21d4758
sherlockes <sherlockes@yahoo.es>
2022-01-26 00:00:37 +0100

    Update

--- bash/deblog_old.sh
+++ bash/deblog_old.sh
@@ -4,15 +4,17 @@
 #Script Name: deblog.sh
 #Description: Generación de entorno de desarrollo para sherblog.pro
 #Args: N/A
-#Creation/Update: 20211214/20220125
+#Creation/Update: 20211214/20220110
 #Author: www.sherblog.pro                                                
 #Email: sherlockes@gmail.com                                           
 ###################################################################
 
-
-blog_dev="$(pwd)/sherlockes.gitlab.io"
-blog_repo="git@gitlab.com:sherlockes/sherlockes.gitlab.io.git"
-
+blog_dev=~/sherblog_dev
+blog_repo="https://github.com/sherlockes/sherblog.git"
+host_prod="rpi"
+blog_prod="rpi:~/sherblog/"
+publish_command='./SherloScripts/bash/publish.sh'
+content_local=~/Google_Drive/Sherblog/content/
 
 # -------------------------------------------------------
 # Función para mostrar las diferentes opciones en pantalla
@@ -22,11 +24,11 @@ show_menus() {
     echo "------------------------------------------------"	
     echo "--------------  www.sherblog.pro  --------------"
     echo "------------------------------------------------"
-    echo "-- 1. Clonado repositorio"
-    echo "-- 2. Actualizar repositorio"
-    echo "-- 3. Guardar repositorio"
-    echo "-- 4. Lanzar el servidor de Hugo"
-    echo "-- 5. Borrar y salir"
+    echo "-- 1. Clonado desde repositorio de GitHub"
+    echo "-- 2. Clonado desde Raspberry"
+    echo "-- 3. Copiar el contenido (Post y Vértices)"
+    echo "-- 4. Copiar configuración"
+    echo "-- 5. Salir"
     echo "------------------------------------------------"
 }
 
@@ -37,10 +39,10 @@ read_options(){
     local choice
     read -p "Selecciona una opción [ 1 - 8] " choice
     case $choice in
-	1) clonar_git ;;
-	2) actualizar_repo ;;
-	3) guardar_repo ;;
-	4) lanzar_servidor ;;
+	1) clonar_github ;;
+	2) clonar_rpi ;;
+	3) guardar_contenido ;;
+	4) guardar_config ;;
 	5) salir ;;
 	*) echo -e "...Error..." && sleep 2
     esac
@@ -49,7 +51,7 @@ read_options(){
 # -------------------------------------------------------
 # Función para clonar el repositorio en la carpeta dev
 # -------------------------------------------------------
-clonar_git(){
+clonar_github(){
     clear
     
     echo "Eliminando las carpetas generadas..."
@@ -59,33 +61,53 @@ clonar_git(){
     mkdir $blog_dev
 
     echo "Clonando contenido de GitHub..."
-    git clone --recurse-submodules $blog_repo $blog_dev
+    git clone $blog_repo $blog_dev
 
     lanzar_servidor
 }
 
 # -------------------------------------------------------
-# Actualizar el contenido de desarrollo en el dir local
+# Función para clonar el dir de produción en el de dev
 # -------------------------------------------------------
-actualizar_repo(){
+clonar_rpi(){
     clear
     
-    echo "Actualizando el contenido del repositorio..."
-    cd $blog_dev
-    git fetch
+    echo "Eliminando las carpetas generadas..."
+    rm -rf $blog_dev
+
+    echo "Creando arbol de directorios..."
+    mkdir $blog_dev
+
+    echo "Clonando contenido de la Raspberry..."
+    rsync -aqzhe ssh --exclude '.git' $blog_prod $blog_dev
+
+    lanzar_servidor
 }
 
 # -------------------------------------------------------
 # Guardar el contenido de desarrollo en el dir local
 # -------------------------------------------------------
-guardar_repo(){
+guardar_contenido(){
     clear
     
-    echo "Guardando los cambio en el repositorio..."
+    echo "Actualizando el contenido del Blog..."
     cd $blog_dev
-    git add .
-    git commit -m "Update"
-    git push
+    rsync -aqzhe ssh --exclude '.git' "$blog_dev/content/" $content_local
+    echo "Publicando los cambios..."
+    ssh $host_prod $publish_command
+}
+
+# -------------------------------------------------------
+# Guardar la configuración en el dir de produción
+# -------------------------------------------------------
+guardar_config(){
+    clear
+    
+    echo "Actualizando la configuración del Blog..."
+    cd $blog_dev
+    rsync -aqzhe ssh --exclude '.git' --exclude 'content' "$blog_dev/" $blog_prod
+    echo "Publicando los cambios..."
+    ssh rpi './SherloScripts/bash/publish.sh'
 }
 
 # -------------------------------------------------------
@@ -94,9 +116,8 @@ guardar_repo(){
 lanzar_servidor(){
     echo "Publicando y arrancando el servidor de Hugo..."
     xdg-open http://localhost:1313
-    echo $blog_dev
     cd $blog_dev
-    hugo server --baseURL=http://localhost:1313
+    hugo server
 }
 
 # -------------------------------------------------------
@@ -104,7 +125,6 @@ lanzar_servidor(){
 # -------------------------------------------------------
 salir(){
     echo "Eliminando las carpetas generadas..."
-    sleep 1
     rm -rf $blog_dev
     echo "Script terminado¡¡¡"
     exit 0
--- bash/publish.sh
+++ bash/publish.sh
@@ -4,12 +4,10 @@
 # Script Name: Actualización de www.sherblog.pro en Gitlab.io
 # Description:
 #	- Sincroniza Google Drive con las carpetas locales
-#	- Añade una cabecera a los archivos que no la tienen
 #	- Actualiza los archivos de la nube a los nuevos con cabecera
-#	- Genera la web estática
-#	- Sube la web a GitHub
+#	- Sube la web a GitLab
 # Args: N/A
-# Creation/Update: 20180901/20220119
+# Creation/Update: 20180901/20220125
 # Author: www.sherblog.pro                                                
 # Email: sherlockes@gmail.com                                           
 ############################################################################
@@ -23,94 +21,6 @@ inicio=$( date +%s )
 mensaje=$'Actualización del <a href="https://sherblog.pro">blog</a> mediante <a href="https://raw.githubusercontent.com/sherlockes/SherloScripts/master/bash/publish.sh">publish.sh</a>\n'
 mensaje+=$'- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n'
 
-add_header(){ 
-    # Calculo de lineas del archivo y líneas de contenido
-    lines_total=$(wc -l $1 | awk '{print $1}')
-    lines_content=$(expr $lines_total - 2)
-    echo $1 tiene $lines_total líneas con un contenido de $lines_content líneas.
-    echo Colocando la cabecera a $1
-
-    # Asigna la primera línea a "short_title", elimina los espacios y los convierte en guión bajo.
-    short_title=$(head -n 1 $1 | sed 's/ /_/g') 
-    echo El título será: $short_title
-
-    # Asigna la segunda línea a "long_title"
-    long_title=$(sed '2q;d' $1)
-    echo El título largo será: $long_title
-
-    # Asigna el resto a todo el contenido y divide en resumen y contenido
-    all_content=$(tail -$lines_content $1)
-    summary=$(echo $all_content| cut -d'.' -f 1)"."
-    echo El resumen será: $summary
-    content=$(echo $all_content| cut -d'.' -f2-)
-    echo El contenido será: $content
-
-    # Establece el nombre para el nuevo archivo
-    file_name="$(date +%Y%m%d)_$short_title.md"
-    echo El nombre del archivo será: $file_name
-
-    # Creación del archivo definitivo
-    echo "---" >> $file_name
-    echo "title: \"$long_title\"" >> $file_name
-    echo "date: \"$(date +%Y-%m-%d)\"" >> $file_name
-    echo "creation: \"$(date +%Y-%m-%d)\"" >> $file_name
-    echo "description: \"$long_title\"" >> $file_name
-    echo "thumbnail: \"/images/$(date +%Y%m%d)_"$short_title"_00.jpg\"" >> $file_name
-    echo "disable_comments: true" >> $file_name
-    echo "authorbox: false" >> $file_name
-    echo "toc: false" >> $file_name
-    echo "mathjax: false" >> $file_name
-    echo "categories:" >> $file_name
-    echo "  - \"uncategorized\"" >> $file_name
-    echo "tags:" >> $file_name
-    echo "  - \"untagged\"" >> $file_name
-    echo "draft: true" >> $file_name
-    echo "weight: 5" >> $file_name
-    echo "---" >> $file_name
-    echo $summary >> $file_name
-    echo "<!--more-->" >> $file_name
-    echo $content >> $file_name
-    echo "[Image-01]: /images/$(date +%Y%m%d)_"$short_title"_01.jpg" >> $file_name
-
-    mensaje+=$'Se ha generado un nuevo archivo de post...\n'
-}
-
-min_time(){
-    echo "Calculando el tiempo desde la última modificación de $1 ..."
-    last_mod_sg=$(date +%s -r $1)
-    now_sg=$(date +%s)
-    mins_last_mod=$(((now_sg - last_mod_sg)/60))
-
-    if [ $mins_last_mod -gt $mins_mod ]
-    then
-	return 0
-    else
-	return 1
-    fi
-    
-}
-
-scan_posts(){
-
-    #Cambio al directorio de contenidos
-    echo Escaneando post para encabezar....
-    cd $dir_post
-
-    #Busca archivos sin cabecera para añadirle una genérica
-    grep -LR -e "title: " -e "date: " $dir_post |
-    while read fname
-    do
-        #Inserta la cabecera si ha pasado el tiempo mínimo y borra el original
-        if min_time "$fname"
-        then
-	    add_header "$fname"
-            rm $fname
-        else
-	    echo No ha pasado el tiempo suficiente para "$fname"
-        fi
-    done
-}
-
 comprobar(){
 
     if [ $1 -eq 0 ]; then
@@ -123,7 +33,7 @@ comprobar(){
 }
 
 # Comprueba si hay contenido para actualizar
-if rclone check --one-way Sherlockes_GD:/Sherblog/content/ /home/pi/sherlockes.gitlab.io/content/; then
+if rclone check Sherlockes_GD:/Sherblog/content/ /home/pi/sherlockes.gitlab.io/content/; then
     echo No hay cambios que actualizar...
 else
     echo Actualizando los cambios...
@@ -149,17 +59,18 @@ else
     crontab -l > /home/pi/sherlockes.gitlab.io/static/files/crontab.txt
     comprobar $?
 
-    mensaje+='Publicación de la web en Hugo................'
-    cd ~/sherlockes.gitlab.io
-    /usr/local/bin/hugo
-    comprobar $?
-
     # Sube los cambios generados en la web a GitHub
     mensaje+='Actualización de la web en Github.com....'
     cd ~/sherlockes.gitlab.io
-    git add --all
+    git add .
     git commit -m "Update"
-    git push #-u origin master
+    git push
+    comprobar $?
+
+    # Sincroniza el repo con google drive
+    mensaje+='Actualización del contenido a Google Drive....'
+    rclone sync -v /home/pi/sherlockes.gitlab.io/static/ Sherlockes_GD:/Sherblog/static/
+    rclone sync -v /home/pi/sherlockes.gitlab.io/content/ Sherlockes_GD:/Sherblog/content/
     comprobar $?
 
     # Envia el mensaje de telegram con el resultado
@@ -170,10 +81,5 @@ else
     
     $notificacion "$mensaje"
 fi
-
-scan_posts
-
-# Sincroniza los archivos de la nube con los modificados en local
-rclone sync -v --exclude 'public' --exclude 'themes' --exclude '.git' /home/pi/sherlockes.gitlab.io/content/ Sherlockes_GD:/Sherblog/content/
 
 exit 0
