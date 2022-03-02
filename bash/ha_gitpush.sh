4b5e48cc7d7707752dc520a79f4825058d7671d7
sherlockes <sherlockes@yahoo.es>
2022-03-02 13:13:17 +0100

    Update

---
+++ bash/ha_gitpush.sh
@@ -0,0 +1,10 @@
+cd /config
+
+# Add all files to the repository with respect to .gitignore rules
+git add .
+
+# Commit changes with message with current date stamp
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+
+# Push changes towards GitHub
+git push -u origin master
