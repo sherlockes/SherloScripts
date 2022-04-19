5a89618e4073aec6c6bf7a8af127b8d0d50a6b89
sherlockes <sherlockes@yahoo.es>
2022-04-20 00:00:17 +0200

    Update

--- python/reto_python/main.py
+++ python/reto_python/main.py
@@ -10,6 +10,7 @@
 # Email: sherlockes@gmail.com
 ##################################################################
 
+import os
 from pathlib import Path
 from xdg import xdg_config_home
 from configurator import Configurator
@@ -21,8 +22,12 @@ def main(app, config):
     path = Path(xdg_config_home()) / app
     configurator = Configurator(path, config)
     data = configurator.read()
-    print(data['directorio'])
-    list_images(Path(data['directorio']))
+
+    
+    if len(data['directorios'])>0:
+        for carpeta in data['directorios'].values():
+            print('===', carpeta['in'], '===')
+            list_images(Path(carpeta["in"]))
 
 if __name__ == '__main__':
     APP = "diogenes"
