93a5cb29b9f5034654f2530b9def21d4c5a8d353
Sherlockes <sherlockes@yahoo.es>
2023-12-16 11:04:04 +0000

    Update

--- bash/webdav.sh
+++ bash/webdav.sh
@@ -10,5 +10,5 @@
 ###################################################################
 
 sleep 30
-rclone serve webdav Sherlockes78_UN3_en: --addr 192.168.10.202:5005 --read-only
+rclone serve webdav Sherlockes78_GD: --addr 192.168.10.210:5005 --read-only
 #rclone serve webdav Sherlockes78_UN_en: --addr 192.168.10.202:5005 --read-only
