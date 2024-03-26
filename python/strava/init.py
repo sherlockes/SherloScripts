63ea08ebbf76bb4e0c380d468d989ad3b054c953
Sherlockes <sherlockes@gmail.com>
2024-03-26 12:10:41 +0100

    Update

--- python/strava/init.py
+++ python/strava/init.py
@@ -1,3 +1,3 @@
 from stravalib import Client
 client = Client()
-url = client.authorization_url(client_id=114649,          redirect_uri='http://127.0.0.1:5000/authorization')
+url = client.authorization_url(client_id=114649, redirect_uri='http://127.0.0.1:5000/authorization')
