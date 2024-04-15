6d9ebcf8b73c2d18825e922650ef0f8f960858ab
Sherlockes <sherlockes@gmail.com>
2024-04-15 14:14:24 +0200

    Update

--- bash/yt2pcst.sh
+++ bash/yt2pcst.sh
@@ -103,6 +103,11 @@ buscar_ultimos(){
 	    # Descargando el episodio
 	    descargar_video $id
 	    comprobar $?
+
+	    echo "- Taggeando el vídeo $id"
+	    mensaje+=$"Taggeando vídeo $id . . . . . . "
+	    tag $id $nombre
+	    comprobar $?
 	else
 	    echo "- El episodio $id es corto o ya descargado"
 	fi
@@ -124,6 +129,69 @@ descargar_video(){
 	echo -e "- Añadiendo a la lista de episodios descargados"
 	echo $id | cat - $DESCARGADOS > temp && mv temp $DESCARGADOS
     fi
+}
+
+#----------------------------------------------------------#
+#        Añadir info al archivo de audio de Youtube        #
+#----------------------------------------------------------#
+tag(){
+    local id=${1:?Falta el id del archivo}
+    local artista=${2:?Falta el nombre del artista}
+    local titulo=$(yt-dlp --get-title "https://www.youtube.com/watch?v=$id")
+
+    echo -e "- Taggeando y moviendo el audio."
+
+    # Poniendo título al audio
+    id3v2 -t "$titulo" -a "$CANAL_NOMBRE" -A "Youtube2Podcast" -- "$id.mp3"
+
+    # Añadir el item al listado del feed
+    anadir_item "$id.mp3"
+
+    # Mover a la carpeta mp3
+    mv -- "$id.mp3" mp3
+}
+
+#----------------------------------------------------------#
+#                   Añadir un nuevo item                   #
+#----------------------------------------------------------#
+anadir_item(){
+    echo "- Añadiendo elemento a la lista..."
+    local file=${1:?Falta el nombre del archivo}
+    
+    #local ID_EP="${track%.*}"
+    local ID_EP=$id
+    
+    local TIT_EP=$(ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 -- $file)
+    local ART_EP=$(ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 -- $file)
+
+    local URL_VID
+    local FEC_EP
+
+    URL_VID="https://www.youtube.com/watch?v=$ID_EP"
+    FEC_EP=$(yt-dlp --print "%(upload_date)s" $URL_VID)
+    FEC_EP=$(date -d $FEC_EP +"%Y-%m-%dT%H:%M:%S%:z")
+    FEC_EP=$(date --date "$FEC_EP+14 hours" "+%a, %d %b %Y %T %Z")
+
+    local LEN_EP=$(ffprobe -i $file -show_format -v quiet | sed -n 's/duration=//p')
+    LEN_EP=$(echo ${LEN_EP%.*})
+
+    # crea el "item.xml" con info del episodio
+    cat >> item.xml <<END_ITEM
+    <item>
+      <guid isPermaLink="true">$servidor/$canal/$ID_EP</guid>
+      <title>$TIT_EP</title>
+      <link>$URL_EP</link>
+      <description>$TIT_EP</description>
+      <pubDate>$FEC_EP</pubDate>
+      <author>$ART_EP</author>
+      <content:encoded><![CDATA[<p>Episodio descargado de $servicio.</p>]]></content:encoded>
+      <enclosure length="$LEN_EP" type="audio/mpeg" url="$SERVIDOR/twitch/$canal/mp3/$ID_EP.mp3"/>
+    </item>
+END_ITEM
+
+    # Añade los "items.xml" ya descargado a continuación del "item.xml"
+    cat items.xml >> item.xml
+    mv item.xml items.xml
 }
 
 ################################
