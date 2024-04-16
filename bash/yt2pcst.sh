fa564382bd670eb42fe8a0df46c3f47bc80aede9
Sherlockes <sherlockes@gmail.com>
2024-04-16 17:21:13 +0200

    Update

--- bash/yt2pcst.sh
+++ bash/yt2pcst.sh
@@ -136,10 +136,9 @@ buscar_ultimos(){
 	id=$( echo "$video" |cut -d\/ -f1 )
 	duracion=$( echo "$video" |cut -d\/ -f2 )
 	duracion=${duracion%??}
-	echo $duracion
 
 	# Comprueba si el archivo es de más de 10'
-	if (( $duracion > 600 )) && ! grep -q $id "$DESCARGADOS"; then
+	if ! grep -q $id "$DESCARGADOS"; then
 	    # Descargando el episodio
 	    descargar_video $id
 	    comprobar $?
