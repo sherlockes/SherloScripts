ebb1e5978b7ddcbfc05cd08e500376bc2ff61687
Sherlockes <sherlockes@gmail.com>
2024-04-24 11:58:10 +0200

    Update

--- bash/telegram_V2.sh
+++ bash/telegram_V2.sh
@@ -64,7 +64,7 @@ send_msg() {
 	# URL para enviar mensaje
 	url="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-	mensaje="`Este es un texto monoespaciado.Puedes incluir código aquí.`"
+	mensaje="\`Este es un texto monoespaciado.Puedes incluir código aquí.\`"
 	
 	# Parámetros del mensaje
 	parametros="chat_id=$CHAT_ID&text=$mensaje&parse_mode=Markdown"
