547e95a8c717f35805e2fa4cb5646311c0594888
Sherlockes <sherlockes@gmail.com>
2024-04-26 14:52:10 +0200

    Update

--- bash/yt2pcst.sh
+++ bash/yt2pcst.sh
@@ -337,7 +337,7 @@ echo "######################################"
 echo "## Youtube to Podcast by Sherlockes ##"
 echo "######################################"
 
-tele_msg_title "Un título para un texto"
+tele_msg_title "From youtube to podcast"
 
 cd $yt2pcst_dir
 
