6bbb4d46ef7629530ff36032fd748b2719c0af1e
sherlockes <sherlockes@yahoo.es>
2025-03-05 18:51:21 +0100

    Update

--- bash/radares.sh
+++ bash/radares.sh
@@ -103,9 +103,47 @@ clear(){
     rm -rf $carpeta
 }
 
+download_and_unzip() {
+    local url="https://www.todo-poi.es/radar/GARMIN_RADARES/garminvelocidad%203xx-5xx-6xx,%20Zumo,%20StreetPilot%20c550,%202720,%202820,%207200%20y%207500.zip"
+    local output_file="$HOME/radares.zip"
+    local output_dir="$HOME/radares"
+
+    # Crear el directorio en la home del usuario si no existe
+    mkdir -p "$output_dir"
+
+    # Descargar el archivo en la home del usuario
+    echo "Descargando el archivo en $output_file..."
+    curl -L -o "$output_file" "$url"
+
+    # Verificar si la descarga fue exitosa
+    if [[ $? -ne 0 ]]; then
+        echo "Error al descargar el archivo."
+        return 1
+    fi
+
+    # Descomprimir el archivo en ~/radares/
+    echo "Descomprimiendo el archivo en $output_dir..."
+    unzip "$output_file" -d "$output_dir"
+
+    # Verificar si la extracción fue exitosa
+    if [[ $? -ne 0 ]]; then
+        echo "Error al descomprimir el archivo."
+        return 1
+    fi
+
+    # Eliminar el archivo ZIP después de la extracción (opcional)
+    rm "$output_file"
+
+    echo "Descarga y descompresión completadas exitosamente en $output_dir."
+}
+
+# Llamar a la función
+download_and_unzip
+
+
 
 #### Script principal ####
-download
+#download
 comprobar $?
 unzip
 comprobar $?
