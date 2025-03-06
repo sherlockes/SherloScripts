a5ddae4033714523117b8da1d68f8d887485ece8
sherlockes <sherlockes@yahoo.es>
2025-03-06 12:02:07 +0100

    Update

--- bash/radares.sh
+++ bash/radares.sh
@@ -4,7 +4,7 @@
 #Script Name: Radares
 #Description: Descarga de radares y copia a Google Drive
 #Args: N/A
-#Creation/Update: 20191112/20250305
+#Creation/Update: 20191112/20250306
 #Author: www.sherblog.pro                                                
 #Email: sherlockes@gmail.com                                           
 ###################################################################
@@ -47,18 +47,11 @@ download(){
     curl "https://www.todo-poi.es/radar/GARMIN_RADARES/garmintipo%203xx-5xx-6xx,%20Zumo,%20StreetPilot%20c550,%202720,%202820,%207200%20y%207500.zip" -o $carpeta/radares_2.zip
 }
 
-unzip(){
-    #tele_msg_instr "Unzipping files"
-    #echo "hola undo"
-    #unzip $carpeta/radares_1.zip -d $carpeta/
-    unzip ~/radares/radares_1.zip -d ~/radares/
+unzip_all(){
+    tele_msg_instr "Unzipping files"
+    unzip $carpeta/radares_1.zip -d $carpeta/
 
-    echo "hola"
     cd $carpeta
-    
-    #unzip
-    #echo "adios"
-    #echo "Hola mundo"
     rm -rf $carpeta/PoiLoader
     cp $carpeta/'garminvelocidad 3xx-5xx-6xx, Zumo, StreetPilot c550, 2720, 2820, 7200 y 7500'/*.csv $carpeta
     rm -rf $carpeta/'garminvelocidad 3xx-5xx-6xx, Zumo, StreetPilot c550, 2720, 2820, 7200 y 7500'
@@ -103,58 +96,11 @@ clear(){
     rm -rf $carpeta
 }
 
-download_and_unzip() {
-    local url="https://www.todo-poi.es/radar/GARMIN_RADARES/garminvelocidad%203xx-5xx-6xx,%20Zumo,%20StreetPilot%20c550,%202720,%202820,%207200%20y%207500.zip"
-    local output_file="$HOME/radares.zip"
-    local output_dir="$HOME/radares"
-
-    # Crear el directorio en la home del usuario si no existe
-    mkdir -p "$output_dir"
-
-    # Descargar el archivo en la home del usuario
-    echo "Descargando el archivo en $output_file..."
-    curl -L -o "$output_file" "$url"
-
-    # Verificar si la descarga fue exitosa
-    if [[ $? -ne 0 ]]; then
-        echo "Error al descargar el archivo."
-        return 1
-    fi
-
-    # Descomprimir el archivo en ~/radares/
-    echo "Descomprimiendo el archivo en $output_dir..."
-    unzip -o "$HOME/radares.zip" -d "$HOME/radares"
-
-    # Verificar si la extracción fue exitosa
-    if [[ $? -ne 0 ]]; then
-        echo "Error al descomprimir el archivo."
-        return 1
-    fi
-
-    # Si hay otro ZIP dentro, descomprimirlo también
-    inner_zip=$(find "$output_dir" -type f -name "*.zip")
-    if [[ -n "$inner_zip" ]]; then
-        echo "Encontrado un ZIP dentro: $inner_zip"
-        unzip -o "$inner_zip" -d "$output_dir"
-        rm "$inner_zip"  # Eliminar el ZIP interno después de extraerlo
-    fi
-
-    # Eliminar el archivo ZIP principal después de la extracción (opcional)
-    rm "$output_file"
-
-    echo "Descarga y descompresión completadas exitosamente en $output_dir."
-}
-
-# Llamar a la función
-download_and_unzip
-
-
-
 
 #### Script principal ####
-#download
-#comprobar $?
-#unzip
+download
+comprobar $?
+unzip_all
 #comprobar $?
 #join
 #comprobar $?
