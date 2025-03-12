1320468ce8d21b4dc07a5871b61a257fbf8b84b8
sherlockes <sherlockes@yahoo.es>
2025-03-12 11:47:53 +0100

    Update

--- bash/backup_dockers.sh
+++ bash/backup_dockers.sh
@@ -4,8 +4,8 @@
 #Script Name: backup_dockers.sh
 #Description: Descripción
 #Args: N/A
-#Creation/Update: 20250225/20250225
-#Author: www.sherblog.pro                                             
+#Creation/Update: 20250225/20250312
+#Author: www.sherblog.es                                             
 #Email: sherlockes@gmail.com                               
 ###################################################################
 
@@ -13,42 +13,28 @@
 ####       Variables        ####
 ################################
 
-FOLDER_TO_BACKUP="~/dockers"  # Cambia esta ruta
-REMOTE_NAME="Sherlockes_Mega"
-REMOTE_PATH="backup_dockers"
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_NAME="backup_$(basename "$FOLDER_TO_BACKUP")_$TIMESTAMP.tar.gz"
-BACKUP_PATH="/tmp/$BACKUP_NAME"
+export RCLONE_CONFIG="/home/sherlockes/.config/rclone/rclone.conf"
 
-
-################################
-####      Dependencias      ####
-################################
-
-
-################################
-####       Funciones        ####
-################################
-
+REPO="rclone:Sherlockes_Mega:docker_backups"
+PASSWORD_FILE="/home/sherlockes/.restic-password"
 
 ################################
 ####    Script principal    ####
 ################################
-# Comprimir la carpeta
-echo "Comprimiendo $FOLDER_TO_BACKUP en $BACKUP_PATH..."
-sudo tar -czf "$BACKUP_PATH" -C "$FOLDER_TO_BACKUP" .
 
-# Subir a rclone
-echo "Subiendo $BACKUP_NAME a $REMOTE_NAME:$REMOTE_PATH..."
-rclone copy "$BACKUP_PATH" "$REMOTE_NAME:$REMOTE_PATH/"
+# Copiar la carpeta del docker intrubot en RPI
+echo "Copiando Intrubot config desde RPI"
+rsync -avz --progress --recursive --mkpath pi@192.168.10.210:/home/pi/dockers/i>
 
-# Verificar si la subida fue exitosa
-if [ $? -eq 0 ]; then
-    echo "Backup subido con éxito."
-    sudo rm "$BACKUP_PATH"
-else
-    echo "Error en la subida del backup."
-fi
+# Realizar backup
+echo "Iniciando backup de ~/dockers: $(date)"
+sudo -E restic -r $REPO --password-file $PASSWORD_FILE backup ~/dockers --exclu>
+
+# Política de retención (opcional)
+echo "Aplicando política de retención"
+sudo -E restic -r $REPO --password-file $PASSWORD_FILE forget --keep-daily 7 -->
+
+
 
 
 
